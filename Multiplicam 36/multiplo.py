c9cc0044fcc355c71bd89722f41ae0bec6b211e3
Michelle-Freitas <michelle.afreitas7@gmail.com>
2022-11-01 16:50:38 -0300

    teste

--- Multiplicam 36/multiplo.py
+++ Multiplicam 36/multiplo.py
@@ -1,10 +1,17 @@
-lista = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+lista = [ 1, 2, 3, 4, 5, 6 ]
 
 def verificar(lista):
-    return (sorted(lista))
+    teste = []
+    for i in range(0, len(lista)):
+        for j in range (0, len(lista)):
+            tentativa = lista[i]*lista[j]
+            teste.append(tentativa)
+
+    if 36 in teste:
+        return True
+    else:
+        return False
 
 
+#print(verificar(lista))
 
-
-
-print(verificar(lista))
